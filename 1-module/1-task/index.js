48a6329f072a2a2cad01f9910d58084d2bd8609d
Vladimir Vavshchik <56423781+SineYlo@users.noreply.github.com>
2023-05-03 12:01:00 +1000

    feat: a function calculating the factorial was written

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,17 @@
 function factorial(n) {
-  // ваш код...
+  if (n === 0) {
+    return 1;
+  }
+
+  if (n < 0) {
+    return NaN;
+  }
+
+  let total = 1;
+
+  for (let i = 2; i <= n; i++) {
+    total *= i;
+  }
+
+  return total;
 }
